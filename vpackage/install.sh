524fed6551b26c9658d259731de813f52ed7c0e9
VIKTOR CHIARCOS <viktor.chiarcos@gmail.com>
2026-02-08 10:09:55 +0000

    Update install.sh

--- vpackage/install.sh
+++ vpackage/install.sh
@@ -3,8 +3,8 @@
 echo "Git Center von @viktor-chiarcos installation wird gestartet..."
 echo "Installation nach ~/bin wird gestartet"
 if [ -e "~/bin" ]
-then wget https://raw.githubusercontent.com/viktor-chiarcos/git-center/refs/heads/master/git-center
-else mkdir ~/bin ; wget https://raw.githubusercontent.com/viktor-chiarcos/git-center/refs/heads/master/git-center
+then cd ~/bin ; wget https://raw.githubusercontent.com/viktor-chiarcos/git-center/refs/heads/master/git-center
+else mkdir ~/bin ; cd ~/bin ; wget https://raw.githubusercontent.com/viktor-chiarcos/git-center/refs/heads/master/git-center
 fi
 echo "Installation fertig"
 echo "Starte git-center"
